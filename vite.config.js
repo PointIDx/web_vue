cea4c6e9ec5b4c23d20fccd16d54106828e691a7
Prince Merveil ONDONDA <pondonda@gmail.com>
2025-06-14 09:08:25 +0200

    add shadcn

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -10,6 +10,18 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    tailwindcss({
+      config: {
+        content: [
+          './index.html',
+          './src/**/*.{vue,js,ts,jsx,tsx}',
+        ],
+        theme: {
+          extend: {},
+        },
+        plugins: [],
+      }
+    })
   ],
   resolve: {
     alias: {
